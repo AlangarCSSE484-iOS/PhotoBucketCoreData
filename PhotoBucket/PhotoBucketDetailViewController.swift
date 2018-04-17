70e0a03deacd0590ec6e6f23061b918f253dff73
Vibha Alangar <alangavr@rose-hulman.edu>
2018-04-17 02:05:06 -0400

    add functionality works!

--- PhotoBucket/PhotoBucketDetailViewController.swift
+++ PhotoBucket/PhotoBucketDetailViewController.swift
@@ -13,6 +13,7 @@ class PhotoBucketDetailViewController: UIViewController {
     
     @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    var photoToAdd : Photo?
     
     
     
@@ -20,11 +21,36 @@ class PhotoBucketDetailViewController: UIViewController {
         super.viewDidLoad()
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.captionLabel.text = self.photoToAdd?.caption
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let imgString = photoToAdd?.imageURL {
+            if let imgUrl = URL(string: imgString) {
+                DispatchQueue.global().async { // Download in the background
+                    do {
+                        let data = try Data(contentsOf: imgUrl)
+                        DispatchQueue.main.async { // Then update on main thread
+                            self.imageView.image = UIImage(data: data)
+                        }
+                    } catch {
+                        print("Error downloading image: \(error)")
+                    }
+                }
+            }
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
 
--- PhotoBucket/PhotoBucketTableViewController.swift
+++ PhotoBucket/PhotoBucketTableViewController.swift
@@ -148,14 +148,18 @@ class PhotoBucketTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == ShowDetailSegueIdentifier {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                (segue.destination as! PhotoBucketDetailViewController).photoToAdd =
+                    photoBucket[indexPath.row]
+            }
+        }
     }
-    */
+ 
 
 }
